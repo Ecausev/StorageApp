ea75c81c5febf4f12728cb8e309fcd4416168d48
Ecausev <ervin.causev@gmail.com>
2018-02-19 22:20:40 +0100

    Pocentni Dizajn

--- StorageApp/Form1.Designer.cs
+++ StorageApp/Form1.Designer.cs
@@ -28,20 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.skladisteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dodajToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.izbrisiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.narudzbeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.noveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(530, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ok";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ime Proizvoda";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.skladisteToolStripMenuItem,
+            this.narudzbeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(861, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // skladisteToolStripMenuItem
+            // 
+            this.skladisteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dodajToolStripMenuItem,
+            this.izbrisiToolStripMenuItem});
+            this.skladisteToolStripMenuItem.Name = "skladisteToolStripMenuItem";
+            this.skladisteToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.skladisteToolStripMenuItem.Text = "Skladiste";
+            this.skladisteToolStripMenuItem.Click += new System.EventHandler(this.skladisteToolStripMenuItem_Click);
+            // 
+            // dodajToolStripMenuItem
+            // 
+            this.dodajToolStripMenuItem.Name = "dodajToolStripMenuItem";
+            this.dodajToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.dodajToolStripMenuItem.Text = "Dodaj";
+            // 
+            // izbrisiToolStripMenuItem
+            // 
+            this.izbrisiToolStripMenuItem.Name = "izbrisiToolStripMenuItem";
+            this.izbrisiToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.izbrisiToolStripMenuItem.Text = "Izbrisi";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.checkBox1.Location = new System.Drawing.Point(384, 117);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Delete";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // narudzbeToolStripMenuItem
+            // 
+            this.narudzbeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.noveToolStripMenuItem,
+            this.stareToolStripMenuItem});
+            this.narudzbeToolStripMenuItem.Name = "narudzbeToolStripMenuItem";
+            this.narudzbeToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.narudzbeToolStripMenuItem.Text = "Narudzbe";
+            // 
+            // noveToolStripMenuItem
+            // 
+            this.noveToolStripMenuItem.Name = "noveToolStripMenuItem";
+            this.noveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.noveToolStripMenuItem.Text = "Nova";
+            // 
+            // stareToolStripMenuItem
+            // 
+            this.stareToolStripMenuItem.Name = "stareToolStripMenuItem";
+            this.stareToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.stareToolStripMenuItem.Text = "Arhiva";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(308, 262);
+            this.ClientSize = new System.Drawing.Size(861, 296);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem skladisteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dodajToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem izbrisiToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolStripMenuItem narudzbeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem noveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stareToolStripMenuItem;
     }
 }
 
--- StorageApp/Form1.cs
+++ StorageApp/Form1.cs
@@ -23,5 +23,25 @@ namespace StorageApp
 
             //Test
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void skladisteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
